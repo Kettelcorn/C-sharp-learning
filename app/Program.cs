31b3cfbc670858b09a0b4986d63cac58c430e662
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2022-08-19 07:31:11 -0700

    Added folder to repo

---
+++ app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+// in terminal, find file you want to start project ("cd 'folder name'" to next, cd ~ to previous)
+// then type dotnet new console -o 'folder name' to make new project
+// to run code, type dotnet run
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> listy = new LinkedList<int>();
+            listy.AddLast(5);
+            listy.AddLast(10);
+            listy.AddLast(75);
+            Console.WriteLine(string.Join(",", listy));
+            Console.WriteLine("Hellow World");
+        }
+    }
+}
